c12fa10cbb539bc8c73863a25cbbcb322272dfab
José Ovando Ricárdez <joseovando26@gmail.com>
2021-08-26 13:54:13 -0500

    Affymetrix miRNAs update

---
+++ 01_ANALISIS_MICROARREGLOS/02_MICROARREGLOS_DE_MIRNAS/01_ANALISIS_POR_PLATAFORMAS/01_EXPERIMENTOS_AFFYMETRIX/GSE32538/00_RSCRIPTS/GSE32538.R
@@ -0,0 +1,279 @@
+#####################################################################
+#Titulo: Pipeline para Affymetrix Multispecies miRNA-1 Array
+#Autor: Jose Antonio Ovando Ricardez
+#Fecha: Mayo/2021
+
+#####################################################################
+# Instalacion de paqueterias
+#####################################################################
+#install.packages("devtools")
+#devtools::install_github("r-lib/remotes")
+#install.packages("BiocManager")
+#BiocManager::install("oligoClasses")
+#BiocManager::install("oligo")
+#BiocManager::install("limma")
+#BiocManager::install("GEOquery")
+#BiocManager::install("pheatmap")
+#BiocManager::install("EnhancedVolcano")
+#BiocManager::install("arrayQualityMetrics")
+#BiocManager::install("ArrayExpress")
+#BiocManager::install("pd.mirna.1.0")
+#Plotting and color options packages
+#install.packages("RColorBrewer")
+#install.packages("ggplot2")
+#install.packages("dplyr")
+#install.packages("tidyr")
+
+#####################################################################
+# Llamado de librerias
+#####################################################################
+library(devtools)
+library(remotes)
+#General Bioconductor packages
+library(oligoClasses)
+library(oligo)
+library(limma)
+library(pheatmap)
+library(tidyverse)
+library(ComplexHeatmap)
+library(circlize)
+library(GEOquery)
+library(arrayQualityMetrics)
+library(EnhancedVolcano)
+library(ArrayExpress)
+library(pd.mirna.1.0)
+#Plotting and color options packages)
+library(RColorBrewer)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+
+#####################################################################
+# Abrimos la tabla con el PhenoData (pd)
+#####################################################################
+pd = read.table("./02_PHENODATA/Phenodata_IPF_VS_CONTROL.prn", header=TRUE, as.is=TRUE)
+pd[,"Filename"] = paste(pd[,"Group"], pd[,"Replicate"], sep=".")
+pd
+
+#####################################################################
+# Leyendo archivos CEL
+#####################################################################
+celList <- list.celfiles("./01_DATOS/DATOS", pattern = '*.CEL.gz', full.names=TRUE, listGzipped=TRUE)
+raw_data <- read.celfiles(celList)
+pData(raw_data) = pd
+sampleNames(raw_data) <- pd$Filename
+
+#####################################################################
+#Analisis de calidad (Imagen de cada array)
+#####################################################################
+for (i in 1:152)
+{
+  name = paste("./04_ANALISIS_DE_CALIDAD/01_IMAGENES/IMAGEN",i,".jpg",sep="")
+  jpeg(name)
+  image(raw_data[,i],main=pd$Filename[i])
+  dev.off()
+}
+
+#####################################################################
+#Analisis de calidad con arrayQualityMetrics (raw_data)
+#####################################################################
+arrayQualityMetrics(expressionset = raw_data[, 1:151],
+                    outdir = "./04_ANALISIS_DE_CALIDAD/02_REPORTE_DE_CALIDAD_RAW_DATA",
+                    force = TRUE,
+                    do.logtransform = TRUE)
+
+#####################################################################
+#Normalizacion
+#####################################################################
+
+norm_data <- oligo::rma(raw_data)
+norm_matrix <- Biobase::exprs(norm_data)
+
+#####################################################################
+#Analisis de calidad con arrayQualityMetrics (norm_data)
+#####################################################################
+arrayQualityMetrics(expressionset = norm_data,
+                    outdir = "./04_ANALISIS_DE_CALIDAD/02_REPORTE_DE_CALIDAD_NORM_DATA",
+                    force = TRUE)
+
+#####################################################################
+#PCA
+#####################################################################
+
+PCA <- prcomp(t(norm_matrix), scale = FALSE)
+
+percentVar <- round(100*PCA$sdev^2/sum(PCA$sdev^2),1)
+sd_ratio <- sqrt(percentVar[2] / percentVar[1])
+
+dataGG <- data.frame(PC1 = PCA$x[,1], PC2 = PCA$x[,2],
+                     Group = pData(norm_data)$Group,
+                     Sample = pData(norm_data)$Sample,
+                     Replicate = pData(norm_data)$Replicate 
+)
+
+pdf("./04_ANALISIS_DE_CALIDAD/PCA_GSE32538.pdf")
+ggplot(dataGG, aes(PC1, PC2, label=row.names(dataGG))) +
+  geom_point(aes( colour = Group))+
+  geom_text(size=1)+
+  ggtitle("GSE32538 | PCA plot of the log-transformed norm expression data") +
+  xlab(paste0("PC1, VarExp: ", percentVar[1], "%")) +
+  ylab(paste0("PC2, VarExp: ", percentVar[2], "%")) +
+  theme(plot.title = element_text(hjust = 0.2))+
+  scale_shape_manual(values = c(4,15)) + 
+  scale_color_manual(values = c("#0000ff", "#fb0007")) + theme(plot.title = element_text(face = "bold"))
+dev.off()
+
+#####################################################################
+# Anotacion
+#####################################################################
+
+gpl <- getGEO('GPL8786')
+
+
+Table <- Table(gpl)[1:7815, c(1, 2, 4, 6)]
+
+#Quitamos todos los otros organismos y nos quedamos solo con miRNAs
+#Porque habian otros RNAs pequeños como snoRNA, etc
+
+Table <- (Table[Table$'Species Scientific Name' == "Homo sapiens",])
+Table <- (Table[Table$'Sequence Type' == "miRNA",])
+
+#Convertimos nuestra matriz de expresion como dataframe
+gene_matrix <- cbind(rownames(norm_matrix),
+                     data.frame(norm_matrix, row.names=NULL))
+
+#Realizamos el merge para unir nuestra anotacion la matriz
+AnnotFinal <- merge(x=Table,y=gene_matrix,by.x='ID',by.y='rownames(norm_matrix)')
+
+#####################################################################
+# Expresion diferencial
+#####################################################################
+
+casos <- as.factor(pd$Group)
+design = model.matrix(~ 0+casos)
+colnames(design) = levels(casos)
+design
+
+#Hacemos la matriz de contrastes
+cont.matrix<- makeContrasts(IPF-Control, levels=design)
+cont.matrix
+
+fit<-lmFit(AnnotFinal[5:144],design)
+fit2= contrasts.fit(fit,cont.matrix)
+fit3= eBayes(fit2)
+
+#Guardar tabla
+table_CD = topTable(fit3, coef=1, number=nrow(fit), sort.by= "none",adjust="fdr")
+
+FinalTable = data.frame(AnnotFinal[,1:4], table_CD, AnnotFinal[,5:144])
+
+write.table(FinalTable, file="./05_TABLA_DE_EXPRESION_DIFERENCIAL/DEG_GSE32538.txt", sep="\t",
+            row.names=F, col.names=T, quote=F)
+
+#####################################################################
+#Volcano
+#####################################################################
+
+#Valores de cada color
+keyvals <- ifelse(FinalTable$logFC <= -0.5 & FinalTable$adj.P.Val <0.05,
+                  '#0000ff',  ifelse(FinalTable$logFC >=0.5 & FinalTable$adj.P.Val <0.05,
+                                     '#fb0007', '#e3deeb'))
+#Nombre de valores de cada color
+keyvals[is.na(keyvals)] <- 'black'
+names(keyvals)[keyvals == '#fb0007'] <- 'Up'
+names(keyvals)[keyvals == '#e3deeb'] <- 'NS'
+names(keyvals)[keyvals == '#0000ff'] <- 'Down'
+
+#Generamos el Volcanoplot
+pdf("./06_GRAFICOS_DE_EXPRESION_DIFERENCIAL/Volcano_GSE32538.pdf")
+EnhancedVolcano(FinalTable,lab = FinalTable$miRNA_ID_LIST,
+                x = 'logFC',
+                y = 'adj.P.Val',
+                ylab = bquote(~adj.P.Val),
+                xlab = bquote(~logFC),
+                ylim = c(0, 3),
+                xlim = c(-1.5, 1.5),
+                pCutoff = 0.05,
+                FCcutoff = 0.5,
+                colCustom = keyvals,
+                cutoffLineType = 'solid',
+                cutoffLineCol = 'coral4',
+                hlineWidth = 0.2,
+                cutoffLineWidth = 0.2,
+                axisLabSize = 8,
+                pointSize = 1.0,
+                colAlpha = 1,
+                legendPosition = 'right',
+                legendLabels = c("NS", expression(LogFC), "adj.P.Val", expression(adj.P.Val ~ and
+                                                                                  ~ logFC)),
+                legendLabSize = 8,
+                title = "     GSE32538 | Volcano plot of differential expression",
+                titleLabSize = 13,
+                subtitleLabSize = 9,
+                captionLabSize = 9,
+                border = 'full',
+                borderWidth = 0.5,
+                borderColour = 'black',
+                caption = NULL,
+                subtitle = "Comparation between IPF vs Control                           pCutoff = 0.05,     FCcutoff = 0.5",
+                labSize = 1.0)
+dev.off()
+
+##########################################################################################################################################
+#Filtrado de datos y resulados
+##########################################################################################################################################
+
+#Aqui estableceremos nuestros valores de corte, en este caso usamos 0.05 padj y 0.8 de logFC
+
+##########################################################################################################################################
+
+#Filtrado de datos
+data_filtered <- FinalTable %>% filter(adj.P.Val < 0.05 & (logFC >0.5 | logFC < -0.5 ))
+data_filtered <- data_filtered %>% drop_na
+
+#Cantidad de genes expresados diferencialmente
+nrow(data_filtered)
+
+#Exportamos la lista filtrada
+write.table(data_filtered, file="./07_RESULTADOS/DEG_FILTER_GSE32538.txt", sep="\t", row.names=F, col.names=T, quote=F)
+
+
+#####################################################################
+#Heatmap
+#####################################################################
+
+#Calculo de valor Z
+cal_z_score <- function(x){
+  (x - mean(x)) / sd(x)
+}
+dataZ <- t(apply(data_filtered[,11:150], 1, cal_z_score))
+data_filtered2 <- cbind(data_filtered[,1:10], dataZ[,96:140], dataZ[,1:95])
+data_filtered3 <- data_filtered2 %>% drop_na
+
+#Seleccionar color
+col_fun <- colorRamp2(seq(min(data_filtered3[,11:150]), max(data_filtered3[,11:150]), length = 3), c("#0000ff", "white", "#fb0007"))
+col_fun
+
+rwb <- colorRampPalette(colors = c("blue", "white", "red"))(30)
+
+#Legends
+lgd <- Legend(col_fun = col_fun, title = "Row Z-Score")
+
+#Heatmap sin genes
+pdf("./06_GRAFICOS_DE_EXPRESION_DIFERENCIAL/Heatmap_GSE32538.pdf")
+Heatmap(as.matrix(data_filtered3[,11:150]),
+        name = "Z-Score", column_title = "GSE32538 | Differential gene expression heatmap",  column_title_gp = gpar(fontsize = 13, fontface = "bold"),
+        col = rwb,
+        column_order = order(as.numeric(gsub("column", "", colnames(data_filtered3[,11:150])))),
+        clustering_distance_rows = "euclidean",
+        row_names_gp = gpar(fontsize = 0),
+        column_names_gp = gpar(fontsize = 3, fontface = "bold"),
+        clustering_method_rows = "ward.D2",
+        width = unit(9, "cm"),
+        height = unit(15, "cm"))
+dev.off()
+
+#####################################################################
+#End
+#####################################################################
