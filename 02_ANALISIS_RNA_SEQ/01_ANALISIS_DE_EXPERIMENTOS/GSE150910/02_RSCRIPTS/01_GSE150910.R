28b0989b7d31f1aede6131a278783382057d999d
José Ovando Ricárdez <joseovando26@gmail.com>
2021-08-26 20:36:37 -0500

    RNA-SEQ UPDATE GSE52463

---
+++ 02_ANALISIS_RNA_SEQ/01_ANALISIS_DE_EXPERIMENTOS/GSE150910/02_RSCRIPTS/01_GSE150910.R
@@ -0,0 +1,288 @@
+##########################################################################################################################################
+#Titulo: Pipeline para expresion diferencial usando conteos de GEO
+#Autor: Jose Antonio Ovando Ricardez
+#Fecha: Agosto/2021
+
+##########################################################################################################################################
+# Instalacion de paqueterias
+##########################################################################################################################################
+
+#Paquetes generales de Bioconductor
+#BiocManager::install("ComplexHeatmap")
+#BiocManager::install("circlize")
+#BiocManager::install("EnhancedVolcano")
+#BiocManager::install("DESeq2")
+#BiocManager::install("org.Hs.eg.db")
+#BiocManager::install("AnnotationDbi")
+
+#Plotting y opciones de coloress
+#install.packages("RColorBrewer")
+#install.packages("ggplot2")
+#install.packages("dplyr")
+#install.packages("tidyr")
+
+##########################################################################################################################################
+# Llamado de librerias
+##########################################################################################################################################
+
+library(devtools)
+library(remotes)
+
+#Paquetes generales de bioconductor
+library(tidyverse)
+library(GEOquery)
+library(ComplexHeatmap)
+library(circlize)
+library(EnhancedVolcano)
+library(DESeq2)
+library(AnnotationDbi)
+library(org.Hs.eg.db)
+
+#Plotting y opciones de color
+library(RColorBrewer)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+##########################################################################################################################################
+#01.- Cargar phenodata
+##########################################################################################################################################
+
+#Cargamos el archivo de Phenodata, el cual contiene los datos sobre las
+#muestras, este archivo fue creado en excel, también se puede usar GEOQuery para
+#este paso
+
+Phenodata <- read.table("./02_RSCRIPTS/Phenodata.txt", stringsAsFactors = F, sep = "", header =  T)
+
+##########################################################################################################################################
+#02.- Cargar nuestra matriz de conteos
+##########################################################################################################################################
+
+#Creamos un objeto que va almacenar nuestra matriz de conteo descargada del GEO,
+#aqui los datos venian ya en una sola tabla, en caso de venir las cuentas por separado
+#esto puede variar. Es importante solo quedarnos con los grupos que necesitaremos
+#En este caso en particular se quitaron todas las columnas que contenian al grupo
+#"chp" porque no era parte del estudio.
+
+##########################################################################################################################################
+
+#Objeto que contiene nuestros conteos
+raw_counts <- read.delim(("./01_Counts/GSE150910_counts.csv"), stringsAsFactors = F, sep = ",", row.names = 1)
+
+#Filtrado con  el paquete dplyr de nuestros grupos que no son de nuestro estudio
+raw_counts <- raw_counts %>%
+  dplyr::select(-contains("chp"))
+
+#Convertimos el objeto a una matriz
+raw_counts <- as.matrix(raw_counts)
+
+
+##########################################################################################################################################
+#03.- Union de nuestro Phenodata con la matriz de conteos
+##########################################################################################################################################
+
+#Tenemos que generar un objeto que almacene el Phenodata y conteos, hay varias maneras de hacer
+#esto, en este pipeline se muestra una manera sencilla de realizarlo
+
+##########################################################################################################################################
+
+#Indicamos que los nombres de filas del Phenodata van a ser los nombres de la columna SampleName
+rownames(Phenodata) <- Phenodata$SampleName
+
+#Aqui verificamos si se han aplicado los cambios y si los nombres de filas del Phenodata y
+#el nombre de las columnas de la matriz de conteos sea el mismo
+
+all(rownames(Phenodata) %in% colnames(raw_counts))
+
+all(colnames(raw_counts) %in% rownames(Phenodata))
+
+#Indicamos que los grupos que estan almacenados en la columna de Treatment son un factor
+Phenodata$Group <- as.factor(Phenodata$Group)
+
+#Generamos el objeto "dds", el cual contenda la informacion de los conteos, el phenodata y
+#los grupos que deseamos evaluar para la posterior expresion diferencial
+
+dds = DESeqDataSetFromMatrix(countData = raw_counts,
+                             colData = Phenodata,
+                             design = ~Group)
+
+##########################################################################################################################################
+#04.- PCA
+##########################################################################################################################################
+
+#Parea generar el PCA plot tenemos que normalizar los datos, para esto cuando son menos de 30
+#muestras se recomienda transformar por rlog y cuando son mas de 30 es mejor usar vst
+
+##########################################################################################################################################
+
+#Transformamos los datos por vst
+rld <- vst(dds, blind=FALSE)
+print(rld)
+
+#Generamos los datos para el PCA
+pcaData <- plotPCA(rld, intgroup = "Group", returnData = TRUE)
+print(pcaData)
+
+#Convertimos las coordenadas a porcentajes
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+
+#Generamos el PCA plot
+pdf("./03_GRAFICOS/PCA_GSE150910.pdf")
+ggplot(pcaData, aes(x = PC1, y = PC2, label=pcaData$name, color = Group)) +
+  geom_point(size = 2) +
+  geom_text(size=2)+
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  ggtitle("PCA plot of the log-transformed expression data") +
+  scale_shape_manual(values = c(4,15)) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_color_manual(values = c("#fa1d19", "#1937fa")) + theme(legend.title = element_text(face = "bold",                                                                                           family = "serif"))
+dev.off()
+
+##########################################################################################################################################
+#05.- Expresion diferencial con DEseq2
+##########################################################################################################################################
+
+#Aqui utilizamos la funcion de DESeq2 llamada DESeq en la que tenemos que seleccionar nuestro
+#objeto "dds" el cual almacena los conteos, el phenodata y los grupos. DESeq2 realiza varias
+#tareas en una misma funcion
+
+##########################################################################################################################################
+
+#Llevamos acabo la expresion diferencial con la funcion DESeq
+dds <- DESeq(dds)
+
+#Especificamos el contraste entre los grupos que deseemos comparar
+res <- results(dds, contrast=c("Group", "IPF", "Control"))
+
+#Generamos un dataframe con los resultados
+table_final <- data.frame(res)
+
+#Exportamos la tabla con los resultados
+write.table(table_final, file = "./04_TABLAS/DEG_GSE150910.txt", quote = F, row.names = T, sep="\t", col.names = T)
+
+
+##########################################################################################################################################
+#06.- Filtrado de datos y resulados
+##########################################################################################################################################
+
+#Aqui estableceremos nuestros valores de corte, en este caso usamos 0.05 padj y 2 de logFC
+
+##########################################################################################################################################
+
+#Filtrado de datos
+data_filtered <- table_final %>% filter(padj < 0.01 & (log2FoldChange > 1.5 | log2FoldChange < -1.5 ))
+
+#Cantidad de genes expresados diferencialmente
+nrow(data_filtered)
+
+#Exportamos la lista filtrada
+write.table(data_filtered, file="./05_RESULTADOS/DEG_RESULTS_GSE150910.txt", sep="\t", row.names=T, col.names=T, quote=F)
+
+##########################################################################################################################################
+#07.- Volcano plot
+##########################################################################################################################################
+
+#Aqui haremos el grafico del volcan usando el paquete "Enhanced volcano"
+
+##########################################################################################################################################
+
+#Valores de cada color
+keyvals <- ifelse(table_final$log2FoldChange <= -1.5 & table_final$padj <0.01,
+                  '#0000ff',  ifelse(table_final$log2FoldChange >=1.5 & table_final$padj <0.01,
+                                     '#fb0007', '#e3deeb'))
+#Nombre de valores de cada color
+keyvals[is.na(keyvals)] <- 'black'
+names(keyvals)[keyvals == '#fb0007'] <- 'Up'
+names(keyvals)[keyvals == '#e3deeb'] <- 'NS'
+names(keyvals)[keyvals == '#0000ff'] <- 'Down'
+
+#Generamos el Volcanoplot
+pdf("03_GRAFICOS/Volcano_GSE150910.pdf")
+EnhancedVolcano(table_final,lab = rownames(table_final),
+                x = 'log2FoldChange',
+                y = 'padj',
+                ylab = bquote(~padj),
+                xlab = bquote(~log2FoldChange),
+                ylim = c(0, 75),
+                xlim = c(-6, 6),
+                pCutoff = 0.01,
+                FCcutoff = 1.5,
+                colCustom = keyvals,
+                cutoffLineType = 'solid',
+                cutoffLineCol = 'coral4',
+                hlineWidth = 0.2,
+                cutoffLineWidth = 0.2,
+                axisLabSize = 8,
+                pointSize = 1.0,
+                colAlpha = 1,
+                legendPosition = 'bottom',
+                legendLabels = c("NS", expression(log2FoldChange), "padj", expression(padj ~ and
+                                                                                      ~ log2FoldChange)),
+                legendLabSize = 8,
+                title = "                   Volcano plot of differential expression of genes",
+                titleLabSize = 13,
+                subtitleLabSize = 9,
+                captionLabSize = 9,
+                border = 'full',
+                borderWidth = 0.5,
+                borderColour = 'black',
+                caption = "",
+                subtitle = "Comparation between IPF vs Control                                                                 pCutoff = 0.05,     FCcutoff = 1",
+                labSize = 1)
+dev.off()
+
+
+##########################################################################################################################################
+#08.- Heatmap
+##########################################################################################################################################
+
+#Aqui haremos el grafico del volcan usando el paquete "Complexheatmap"
+
+##########################################################################################################################################
+
+#Normalizamos los datos
+cnorm <- counts(dds, normalized = T)
+
+#Aqui estamos ordenando nuestros datos para crear el heatmap
+IPF <- as.data.frame(cnorm) %>% dplyr::select(-contains("control"))
+Control <- as.data.frame(cnorm) %>% dplyr::select(-contains("ipf"))
+
+#Unimos los dos objetos en uno mismo
+cnorm_sort <- data.frame(Control,IPF)
+colnames(cnorm_sort)
+
+#Unimos los datos de conteos normalizados con la tabla de resultados de la expresion diferencial
+table_final_filtered <- data.frame(table_final, cnorm_sort)
+
+ #Realizamos un filtrado en base a los puntos de corte establecidos
+data_filtered <- table_final_filtered %>% filter(padj < 0.01 & (log2FoldChange > 1.5 | log2FoldChange < -1.5 ))
+
+#Calculo de valor Z
+cal_z_score <- function(x){
+  (x - mean(x)) / sd(x)
+}
+dataZ <- t(apply(data_filtered[,7:212], 1, cal_z_score))
+data_filtered2 <- cbind(data_filtered[,1:6], dataZ[,1:103], dataZ[,104:206])
+data_filtered3 <- data_filtered2 %>% drop_na
+
+#Seleccionar color
+col_fun <- colorRamp2(seq(min(data_filtered3[,7:109]), max(data_filtered3[,7:212]), length = 3), c("#0000ff", "white", "#fb0007"))
+col_fun
+
+#Legends
+lgd <- Legend(col_fun = col_fun, title = "Row Z-Score")
+
+#Heatmap sin genes
+pdf("03_GRAFICOS/Heatmap_GSE150910.pdf")
+Heatmap(as.matrix(data_filtered3[,7:212]),
+        name = "Z-Score", column_title = "Differential gene expression heatmap (GSE150910)",  column_title_gp = gpar(fontsize = 13, fontface = "bold"),
+        col = col_fun,
+        column_order = order(as.numeric(gsub("column", "", colnames(data_filtered3[,7:212])))),
+        clustering_distance_rows = "euclidean",
+        row_names_gp = gpar(fontsize = 0),
+        column_names_gp = gpar(fontsize = 1, fontface = "bold"),
+        clustering_method_rows = "ward.D2",
+        width = unit(9, "cm"),
+        height = unit(15, "cm"))
+dev.off()
---
+++ 02_ANALISIS_RNA_SEQ/01_ANALISIS_DE_EXPERIMENTOS/GSE52463/02_RSCRIPTS/GSE52463.R
@@ -0,0 +1,272 @@
+##########################################################################################################################################
+#Titulo: Pipeline para expresion diferencial usando conteos de HTSEQ
+#Autor: Jose Antonio Ovando Ricardez
+#Fecha: Agosto/2021
+
+##########################################################################################################################################
+# Instalacion de paqueterias
+##########################################################################################################################################
+
+#Paquetes generales de Bioconductor
+#BiocManager::install("ComplexHeatmap")
+#BiocManager::install("circlize")
+#BiocManager::install("EnhancedVolcano")
+#BiocManager::install("DESeq2")
+#BiocManager::install("org.Hs.eg.db")
+#BiocManager::install("AnnotationDbi")
+
+#Plotting y opciones de coloress
+#install.packages("RColorBrewer")
+#install.packages("ggplot2")
+#install.packages("dplyr")
+#install.packages("tidyr")
+
+##########################################################################################################################################
+# Llamado de librerias
+##########################################################################################################################################
+
+library(devtools)
+library(remotes)
+
+#Paquetes generales de bioconductor
+library(tidyverse)
+library(ComplexHeatmap)
+library(circlize)
+library(EnhancedVolcano)
+library(DESeq2)
+library(AnnotationDbi)
+library(org.Hs.eg.db)
+
+#Plotting y opciones de color
+library(RColorBrewer)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+##########################################################################################################################################
+
+#Definir la localizacion de la carpeta que contiene los conteos
+directory <- "./01_COUNTS"
+print(directory)
+
+#Visualizar una lista de los archivos de conteos
+sampleFiles <- grep(".txt",list.files(directory),value=TRUE)
+print(sampleFiles)
+
+##########################################################################################################################################
+
+#Importar la tabla del Phenodata, esta fue creada manualmente en excel
+#Donde se incluye el nombre de las muestras y los grupos a los que corresponden
+
+st <- read.table("03_PHENODATA/Phenodata.txt", header = T)
+st
+
+
+#Creamos el objeto de sampleTable en donde vamos a seleccionar como valores
+#de condicion usando los nombres de nuestros grupos de nuestro Phenodata.csv
+
+sampleTable <- data.frame(sampleName = st$SampleName,
+                          fileName = sampleFiles,
+                          condition = st$Group)
+
+print(sampleTable)
+
+##########################################################################################################################################
+
+#Creamos el objeto DEseq con el output de HTSeq
+
+dds <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
+                                  directory = directory,
+                                  design= ~ condition)
+
+print(dds)
+
+##########################################################################################################################################
+#PCA
+##########################################################################################################################################
+
+#Normalizamos por rlog para crear el PCA, usamos este metodo porque son pocas muestras
+rld <- rlog(dds, blind=FALSE)
+
+#Generamos los datos para el PCA
+pcaData <- plotPCA(rld, intgroup = "condition", returnData = TRUE)
+
+#Convertimos las coordenadas a porcentajes
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+
+#Generamos el PCA plot
+
+pdf("04_GRAFICOS/PCA_GSE52463.pdf")
+ggplot(pcaData, aes(x = PC1, y = PC2, label=pcaData$name, color = condition)) +
+  geom_point(size = 3) +
+  geom_text(size=1)+
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  ggtitle("GSE52463 | PCA plot of the log-transformed norm expression data") +
+  theme(plot.title = element_text(hjust = 0.2))+
+  scale_shape_manual(values = c(4,15)) + 
+  scale_color_manual(values = c("#0000ff", "#fb0007")) + theme(plot.title = element_text(face = "bold"))
+dev.off()
+
+##########################################################################################################################################
+#Expresiond diferencial
+##########################################################################################################################################
+
+#Llevamos acabo el test entre dos condiciones
+dds <- DESeq(dds)
+
+res <- results(dds, contrast=c("condition", "IPF", "Control"))
+
+
+##########################################################################################################################################
+#Anotacion
+##########################################################################################################################################
+
+#Aqui seleccionamos las columnas que deseamos incluir en nuestra anotacion
+columns(org.Hs.eg.db)
+
+convertIDs <- function( ids, from, to, db, ifMultiple=c("putNA", "useFirst")) {
+  stopifnot( inherits( db, "AnnotationDb" ) )
+  ifMultiple <- match.arg( ifMultiple )
+  suppressWarnings( selRes <- AnnotationDbi::select(
+    db, keys=ids, keytype=from, columns=c(from,to) ) )
+  if ( ifMultiple == "putNA" ) {
+    duplicatedIds <- selRes[ duplicated( selRes[,1] ), 1 ]
+    selRes <- selRes[ ! selRes[,1] %in% duplicatedIds, ]
+  }
+  return( selRes[ match( ids, selRes[,1] ), 2 ] )
+}
+
+#Lo añadimos a nuestro objeto res
+res$Symbol <- convertIDs(row.names(res), "ENSEMBL", "SYMBOL", org.Hs.eg.db)
+res$Gene_name <- convertIDs(row.names(res), "ENSEMBL", "GENENAME", org.Hs.eg.db)
+res$Gene_type <- convertIDs(row.names(res), "ENSEMBL", "GENETYPE", org.Hs.eg.db)
+
+#Creamos un dataframe con todos los datos finales
+
+table_final <- data.frame(res)
+
+#Eliminamos todos los valores NA de nuestros datos
+table_final <- table_final %>% drop_na
+
+#Extraemos las tablas de nuestros datos
+write.table(table_final, file="05_TABLAS/DEG_GSE52463.txt", sep="\t", row.names=F, col.names=TRUE, quote=F)
+
+##########################################################################################################################################
+#Volcano
+##########################################################################################################################################
+
+#Valores de cada color
+keyvals <- ifelse(table_final$log2FoldChange <= -1 & table_final$padj <0.05,
+                  '#0000ff',  ifelse(table_final$log2FoldChange >=1 & table_final$padj <0.05,
+                                     '#fb0007', '#e3deeb'))
+#Nombre de valores de cada color
+keyvals[is.na(keyvals)] <- 'black'
+names(keyvals)[keyvals == '#fb0007'] <- 'Up'
+names(keyvals)[keyvals == '#e3deeb'] <- 'NS'
+names(keyvals)[keyvals == '#0000ff'] <- 'Down'
+
+#Script del volcano
+pdf("04_GRAFICOS/Volcano_GSE52463.pdf")
+EnhancedVolcano(table_final,lab = table_final$Symbol,
+                x = 'log2FoldChange',
+                y = 'padj',
+                ylab = bquote(~padj),
+                xlab = bquote(~log2FoldChange),
+                ylim = c(0, 10),
+                xlim = c(-5, 5),
+                pCutoff = 0.05,
+                FCcutoff = 1,
+                colCustom = keyvals,
+                cutoffLineType = 'solid',
+                cutoffLineCol = 'coral4',
+                hlineWidth = 0.2,
+                cutoffLineWidth = 0.2,
+                axisLabSize = 8,
+                pointSize = 1.0,
+                colAlpha = 1,
+                legendPosition = 'right',
+                legendLabels = c("NS", expression(log2FoldChange), "adj.P.Val", expression(padj ~ and
+                                                                                  ~ log2FoldChange)),
+                legendLabSize = 8,
+                title = "     GSE52463 | Volcano plot of differential expression",
+                titleLabSize = 13,
+                subtitleLabSize = 9,
+                captionLabSize = 9,
+                border = 'full',
+                borderWidth = 0.5,
+                borderColour = 'black',
+                caption = NULL,
+                subtitle = "Comparation between IPF vs Control                            pCutoff = 0.05,     FCcutoff = 1",
+                labSize = 1.0)
+dev.off()
+
+##########################################################################################################################################
+#Filtrado de datos y resulados
+##########################################################################################################################################
+
+#Aqui estableceremos nuestros valores de corte, en este caso usamos 0.05 padj y 0.8 de logFC
+
+##########################################################################################################################################
+
+#Filtrado de datos
+data_filtered <- table_final %>% filter(padj < 0.05 & (log2FoldChange > 1 | log2FoldChange < -1 ))
+
+#Cantidad de genes expresados diferencialmente
+nrow(data_filtered)
+
+#Exportamos la lista filtrada
+write.table(data_filtered, file="./06_RESULTADOS/DEG_FILTER_GSE52463.txt", sep="\t", row.names=F, col.names=T, quote=F)
+
+
+##########################################################################################################################################
+#Heatmap
+##########################################################################################################################################
+
+#Obtenemos los datos para hacer el heatmap incluyendo los recuentos normalizados
+
+cnorm <- counts(dds, normalized = T)
+
+table_final <-merge(table_final,cnorm,by='row.names',all=TRUE)
+table_final <- table_final %>% drop_na
+
+#Realizamos el filtrado
+
+data_filtered <- table_final %>% filter(padj < 0.05 & (log2FoldChange > 1 | log2FoldChange < -1 ))
+nrow(data_filtered)
+
+#Calculo de valor Z
+cal_z_score <- function(x){
+  (x - mean(x)) / sd(x)
+}
+dataZ <- t(apply(data_filtered[,11:25], 1, cal_z_score))
+data_filtered2 <- cbind(data_filtered[,1:10], dataZ[,1:7], dataZ[,8:15])
+data_filtered3 <- data_filtered2 %>% drop_na
+
+#Seleccionar color
+col_fun <- colorRamp2(seq(min(data_filtered3[,11:25]), max(data_filtered3[,11:25]), length = 3), c("#0000ff", "white", "#fb0007"))
+col_fun
+
+rwb <- colorRampPalette(colors = c("#0000ff", "white", "#fb0007"))(30)
+
+#Legends
+lgd <- Legend(col_fun = col_fun, title = "Row Z-Score")
+
+#Heatmap sin genes
+pdf("./04_GRAFICOS/Heatmap_GSE52463.pdf")
+Heatmap(as.matrix(data_filtered3[,11:25]),
+        name = "Z-Score", column_title = "GSE52463 | Differential gene expression heatmap",  column_title_gp = gpar(fontsize = 13, fontface = "bold"),
+        col = rwb,
+        column_order = order(as.numeric(gsub("column", "", colnames(data_filtered3[,11:25])))),
+        clustering_distance_rows = "euclidean",
+        row_names_gp = gpar(fontsize = 0),
+        column_names_gp = gpar(fontsize = 3, fontface = "bold"),
+        clustering_method_rows = "ward.D2",
+        width = unit(9, "cm"),
+        height = unit(15, "cm"))
+dev.off()
+
+#####################################################################
+#End
+#####################################################################
+
